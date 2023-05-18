5b654a7b08bf3ffa7e827ef9d0066335500cba1b
YANELI_JAZMIN_HERNANDEZ_HERNANDEZ <a.20308051281007@cbtis128.edu.mx>
2023-05-18 21:10:35 +0000

    a

---
+++ lib/screens/boton_flotante.dart
@@ -0,0 +1,59 @@
+//importacion de libreria material
+import 'package:flutter/material.dart';
+
+
+//crea la clase y el estado a cambiar
+class Botonflotante extends StatefulWidget {
+  const Botonflotante({super.key});
+  _Botonflotante createState() => _Botonflotante();
+}
+
+//crea los widgets a mostrar
+class _Botonflotante extends State<Botonflotante> {
+  String txt="";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Boton Flotante'),
+      ),
+      body: Center(
+       child: Column(
+
+         mainAxisAlignment: MainAxisAlignment.center,
+         children: [
+           Text(txt)
+         ],
+       )
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+                FloatingActionButton(
+                    child: Icon(Icons.visibility),
+                    onPressed: () => setState(() => txt="hola mundo ")
+                    //funsion normal
+                   // onPressed: () {
+                     // setState(() {
+                       //txt="Hola mundo";
+                      //});
+                   // },
+                    
+            
+                  ),
+                  FloatingActionButton(
+                    child: Icon(Icons.visibility_off),
+                    onPressed: () => setState(() => txt="")
+                    //funsion normal
+                    //onPressed: () {
+                      //setState(() {
+                      //txt="";
+                      //});
+                    //},
+                  ),
+                  
+        ],
+      ),
+    );
+  }
+}
